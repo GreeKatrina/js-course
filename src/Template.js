84bb2671b152163c2603a44ae8ae2329d2f1e5f5
Katrina Theodosopoulos <katrinaelaine6@gmail.com>
2014-05-30 11:33:06 -0500

    Pass all tests

--- src/Template.js
+++ src/Template.js
@@ -1,3 +1,9 @@
-var render = function(){
-
+var render = function(temp, data){
+  var keys = Object.keys(data);
+  for (var i in keys){
+    var j = "{{" + keys[i] + "}}";
+    var re = new RegExp(j, "g");
+    temp = temp.replace(re, data[keys[i]]);
+  };
+  return temp;
 };
